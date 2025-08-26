b91ea451a1b81de73b34b4f4c8e615d44e74dc8b
CamillaLisboa <camilla.pintolisboa@icloud.com>
2025-08-26 10:09:41 -0300

    Aulas WEB

--- POO/Flask APP/app.py
+++ POO/Flask APP/app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def principal():
-    frutas = ["Morango", "Uva", "Laranja", "Mamão", "Maçã", "Melão", "Abacaxi"]
+    #frutas = ["Morango", "Uva", "Laranja", "Mamão", "Maçã", "Melão", "Abacaxi"]
+    if request.method == "POST":
+        if request.form.get("frutas")
     return render_template("index.html", frutas=frutas)
 
 @app.route('/sobre')
 def sobre():
     notas={"Fulano":5.0, "Beltrano":6.0, "Aluno":7.0, "Sicrano":8.5}
     return render_template("sobre.html", notas=notas)
+
+if __name__ == "__main__":
+    app.run()
