f8d5809d65e8e5301302c4379a7ac92ce97c1061
CamillaLisboa <camilla.pintolisboa@icloud.com>
2025-09-12 08:27:44 -0300

    delete projeto de POO

--- POO/mvc_mysql/config.py
+++
@@ -1,9 +0,0 @@
-DEBUG = True
-
-USERNAME = 'root'
-PASSWORD = 'root'
-SERVER = 'localhost:3306'
-DB = 'library'
-
-sqlalchemy_database_uri = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
-sqlalchemy_track_modifications = False
